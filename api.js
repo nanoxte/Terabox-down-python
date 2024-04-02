48293aba37ab00e20483a25d36447912751b1c60
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-02 16:04:48 +0530

    Update and rename api.js to api.py

--- api.js
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_details(id):
-    try:
-        response = requests.get(f"https://terabox-app-pearl.vercel.app/api?data={id}")
-        return response.json()
-    except Exception as e:
-        print(e)
---
+++ api.py
@@ -0,0 +1,10 @@
+import aiohttp
+
+async def get_details(id):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://terabox-app-pearl.vercel.app/api?data={id}") as response:
+                data = await response.json()
+                return data
+    except Exception as e:
+        print(e)
