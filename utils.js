c7f164358f6c4766cb8a943bc8d8b1607a02be80
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-02 16:07:31 +0530

    utilis.py

--- utils.js
+++ utils.js
@@ -1,15 +1,9 @@
-const sendFile = async (item, ctx) => {
-  if (item) {
-    try {
-      await ctx.replyWithDocument(item);
-    } catch (e) {
-      ctx.replyWithMarkdown(
-        `⚠️ ${e.message}\n\n👉 Try manually downloading from [here](${item})\n\n👉 *Maybe This File Is Too Large Or Cannot Accessible From Terabox*`,
-      );
-    }
-  }
-};
-
-module.exports = {
-  sendFile,
-};
+async def send_file(item, message):
+    if item:
+        try:
+            await message.reply_document(document=item)
+        except Exception as e:
+            await message.reply_text(
+                f"⚠️ {str(e)}\n\n👉 Try manually downloading from [here]({item})\n\n👉 *Maybe This File Is Too Large Or Cannot Accessible From Terabox*",
+                parse_mode="markdown"
+            )
