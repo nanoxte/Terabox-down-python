b8f41db68f68592eb96c7590cdf91c86a278d113
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-10 10:24:17 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import aiohttp
 async def get_details(id):
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://terabox-app-pearl.vercel.app/api?data={id}") as response:
+            async with session.get(f"https://tbox-vids.vercel.app/api?data={id}") as response:
                 data = await response.json()
                 return data
     except Exception as e:
