f6d0e3d35ed9fd6139416f8338b669448bc44047
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-10 11:24:38 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,18 +6,27 @@ async def send_file(item, message):
         response = requests.get(item)
         if response.status_code == 200:
             file_bytes = io.BytesIO(response.content)
-            content_disposition = response.headers.get('content-disposition')
-            if content_disposition:
-                filename_index = content_disposition.find('filename=')
-                if filename_index != -1:
-                    filename = content_disposition[filename_index + len('filename='):]
-                    filename = filename.strip('"')  # Remove surrounding quotes, if any
-                    file_bytes.name = filename
-                    await message.reply_document(document=file_bytes, caption=filename)
+            content_type = response.headers.get('content-type')
+            if content_type:
+                if 'video' in content_type:
+                    await message.reply_video(video=file_bytes)
+                elif 'image' in content_type:
+                    await message.reply_photo(photo=file_bytes)
                 else:
-                    await message.reply_text("Failed to extract filename from content disposition.")
+                    content_disposition = response.headers.get('content-disposition')
+                    if content_disposition:
+                        filename_index = content_disposition.find('filename=')
+                        if filename_index != -1:
+                            filename = content_disposition[filename_index + len('filename='):]
+                            filename = filename.strip('"')  # Remove surrounding quotes, if any
+                            file_bytes.name = filename
+                            await message.reply_document(document=file_bytes, caption=filename)
+                        else:
+                            await message.reply_text("Failed to extract filename from content disposition.")
+                    else:
+                        await message.reply_text("Failed to extract filename from content disposition.")
             else:
-                await message.reply_text("Failed to extract filename from content disposition.")
+                await message.reply_text("Failed to determine the type of the file.")
         else:
             await message.reply_text("Failed to download the file from the provided URL.")
     except Exception as e:
