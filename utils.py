d54e9820ea026b1f6eff71b404476875efb049f1
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-10 11:46:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,9 +16,9 @@ async def send_file(item, message):
             content_type = response.headers.get('content-type')
             if content_type:
                 if 'video' in content_type:
-                    await message.reply_video(video=file_bytes)
+                    await message.reply_video(video=file_bytes, caption=filename)
                 elif 'image' in content_type:
-                    await message.reply_photo(photo=file_bytes)
+                    await message.reply_photo(photo=file_bytes, caption=filename)
                 else:
                     if content_disposition:
                         filename_index = content_disposition.find('filename=')
