5e0b62616b24ed0eedbcab4ced058b796e182b35
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-11 18:48:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from api import get_details
 import requests
