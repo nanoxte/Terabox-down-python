1b82980710c124d71a70684b6fd02446e12efabf
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-11 18:37:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,45 @@ app = Client(
 
 
 
-# /start command handler
-@app.on_message(filters.command("start"))
-def start_command(client, message):
-    message.reply_text("hello you have started me......")
+@Tech_VJ.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    if Config.TECH_VJ_UPDATES_CHANNEL is not None:
+        back = await handle_force_sub(bot, update)
+        if back == 400:
+            return
+    if len(update.command) != 2:
+      
+    # logger.info(update)
+        await AddUser(bot, update)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.TECH_VJ_START_TEXT.format(update.from_user.mention),
+            reply_markup=Translation.TECH_VJ_START_BUTTONS,
+            reply_to_message_id=update.id
+        )
+        return
+    data = update.command[1]
 
-
+    if data.split("-", 1)[0] == "verify":
+        userid = data.split("-", 2)[1]
+        token = data.split("-", 3)[2]
+        if str(update.from_user.id) != str(userid):
+            return await update.reply_text(
+                text="<b>ᴇxᴘɪʀᴇᴅ ʟɪɴᴋ ᴏʀ ɪɴᴠᴀʟɪᴅ ʟɪɴᴋ !</b>",
+                protect_content=True
+            )
+        is_valid = await check_token(bot, userid, token)
+        if is_valid == True:
+            await update.reply_text(
+                text=f"<b>ʜᴇʟʟᴏ {update.from_user.mention} 👋,\nʏᴏᴜ ᴀʀᴇ sᴜᴄᴄᴇssғᴜʟʟʏ ᴠᴇʀɪғɪᴇᴅ !\n\nɴᴏᴡ ʏᴏᴜ ʜᴀᴠᴇ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇss ғᴏʀ ᴀʟʟ ᴜʀʟ ᴜᴘʟᴏᴀᴅɪɴɢ ᴛɪʟʟ ᴛᴏᴅᴀʏ ᴍɪᴅɴɪɢʜᴛ.</b>",
+                protect_content=True
+            )
+            await verify_user(bot, userid, token)
+        else:
+            return await update.reply_text(
+                text="<b>ᴇxᴘɪʀᴇᴅ ʟɪɴᴋ ᴏʀ ɪɴᴠᴀʟɪᴅ ʟɪɴᴋ !</b>",
+                protect_content=True
+            )
 
 
 # Message handler
